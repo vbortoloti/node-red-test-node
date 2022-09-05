531df3ca557269f899678dc943d25c9a36f52a4b
Vitor Bortoloti <bortoloti.vitor@gmail.com>
2022-09-05 17:18:37 -0300

    sd

--- lower-case.js
+++ lower-case.js
@@ -2,18 +2,18 @@ module.exports = function(RED) {
     function LowerCaseNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
-        var gpioCommand = 'python3 '+__dirname+'/print.py';
         var spawn = require("child_process").spawn;
 
         node.on('input', function(msg) {
-            node.child = spawn('python3 ', ['./print.py ',2,2]);
+            node.child = spawn("python",["print.py",,2,2]);
             console.log("Trying to spwn");
-            let output;
-            node.child.stdout.on("data", (data) => {
-                output += data;
+            node.child.stdout.on('data', function (data) {
+                var d = data.toString().trim().split("\n");
+                for (var i = 0; i < d.length; i++) {
+                    console.log(d[i] === '');
+                }
             });
 
-            console.log(output);
             msg.payload = '-output-';
             node.send(msg);
         });
