9a128abb40f4fb59baa9b74bc217e307ab77c9b1
Vitor Bortoloti <bortoloti.vitor@gmail.com>
2022-09-05 17:21:26 -0300

    s

--- lower-case.js
+++ lower-case.js
@@ -4,16 +4,18 @@ module.exports = function(RED) {
         var node = this;
         var spawn = require("child_process").spawn;
 
-        node.on('input', function(msg) {
-            node.child = spawn("python",["print.py",,2,2]);
+        node.child = spawn("python3",["print.py",2,2]);
             console.log("Trying to spwn");
             node.child.stdout.on('data', function (data) {
                 var d = data.toString().trim().split("\n");
                 for (var i = 0; i < d.length; i++) {
                     console.log(d[i] === '');
+                    console.log("kkk")
                 }
             });
 
+        node.on('input', function(msg) {
+            
             msg.payload = '-output-';
             node.send(msg);
         });
